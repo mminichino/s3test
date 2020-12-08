6445ee843ecb337597998a2e40dd76f6bd39854f
Michael Minichino <mminichino@mac.com>
2020-12-08 17:02:35 -0600

    Updates and bug fixes

--- s3test.py
+++ s3test.py
@@ -6,12 +6,15 @@ import getopt
 import sys
 import os
 import datetime
+import time
 import urllib3
 import math
 import string
 import signal
 import random
 import tempfile
+import threading
+import queue
 import boto3
 import botocore
 from botocore.config import Config
@@ -64,14 +67,16 @@ class parse_args:
         self.dataSize = 65536
         self.endPoint = None
         self.verboseFlag = False
+        self.modelFlag = False
         self.opCount = 1
+        self.threadCount = 1
         self.filePrefix = 'objdata'
         self.opType = 'put'
         self.destDir = '/dev/null'
         self.argCount = 0
 
     def parse(self):
-        options, remainder = getopt.getopt(sys.argv[1:], 'o:f:hvc:b:p:s:e:d:', self.arglist)
+        options, remainder = getopt.getopt(sys.argv[1:], 'o:f:hvc:b:p:s:e:d:t:m', self.arglist)
 
         self.argCount = len(options)
         for opt, arg in options:
@@ -89,11 +94,19 @@ class parse_args:
                 self.endPoint = arg
             elif opt in ('-v', '--verbose'):
                 self.verboseFlag = True
+            elif opt in ('-m', '--model'):
+                self.modelFlag = True
             elif opt in ('-c', '--count'):
                 try:
                     self.opCount = int(arg)
                 except ValueError as e:
                     print("Count must be a number.")
+                    sys.exit(1)
+            elif opt in ('-t', '--threads'):
+                try:
+                    self.threadCount = int(arg)
+                except ValueError as e:
+                    print("Threads must be a number.")
                     sys.exit(1)
             elif opt in ('-f', '--fileprefix'):
                 self.filePrefix = arg
@@ -126,13 +139,16 @@ class tester:
         self.endPoint = self.token.endPoint
         self.awsProfile = self.token.awsProfile
         self.destDir = self.token.destDir
+        self.threadCount = self.token.threadCount
         self.percentage = 0
         self.xfer_total = 0
+        self.xfer_progress = 0
         self.current_file = 0
         self.last_file = 0
+        self.status_thread_run = 1
         self.start_time = datetime.datetime.now().replace(microsecond=0)
         self.end_time = datetime.datetime.now().replace(microsecond=0)
-        config = Config(
+        self.awsConfig = Config(
             retries={
                 'max_attempts': 5,
                 'mode': 'standard'
@@ -146,7 +162,7 @@ class tester:
         except botocore.exceptions.ProfileNotFound as e:
             print("Error: %s" % str(e))
             sys.exit(1)
-        self.s3 = self.s3session.client('s3', endpoint_url=self.endPoint, verify=False, config=config)
+        self.s3 = self.s3session.client('s3', endpoint_url=self.endPoint, verify=False, config=self.awsConfig)
 
     def register_start(self):
 
@@ -170,6 +186,23 @@ class tester:
 
         print("File %d of %d in progress, %d%% completed ... " % (self.current_file, self.opCount, self.percentage), end='\r')
 
+    def thread_status_callback(self, number):
+
+        self.xfer_progress += number
+
+    def print_status_thread(self, q):
+
+        while self.status_thread_run == 1:
+            while not q.empty():
+                self.percentage = (q.get() / self.opCount) * 100
+                end_char = '\r'
+                print("File %d of %d in progress, %d%% completed ... " % (self.current_file, self.opCount, self.percentage), end=end_char)
+            time.sleep(1)
+
+        sys.stdout.write("\rOperation Complete.\033[K\n")
+        print("%d files processed." % self.opCount)
+        return
+
     def upload_file(self, file_name, bucket, name=None):
 
         if name is None:
@@ -185,6 +218,29 @@ class tester:
                 print("Transferred %s" % formatSize(round(self.xfer_total * (self.percentage/100))))
             print("Can not upload object %s: %s" % (name, str(e)))
             sys.exit(1)
+
+    def upload_file_thread(self, file_name, bucket, name):
+
+        try:
+            if self.awsProfile:
+                thread_s3session = boto3.Session(profile_name=self.awsProfile,)
+            else:
+                thread_s3session = boto3.Session(profile_name="default",)
+        except botocore.exceptions.ProfileNotFound as e:
+            print("Error: %s" % str(e))
+            sys.exit(1)
+        thread_s3 = thread_s3session.client('s3', endpoint_url=self.endPoint, verify=False, config=self.awsConfig)
+
+        try:
+            response = thread_s3.upload_file(file_name, bucket, name, Callback=self.thread_status_callback)
+        except (botocore.exceptions.ClientError, boto3.exceptions.S3UploadFailedError) as e:
+            if self.percentage > 0:
+                print("")
+                print("Transferred %s" % formatSize(self.xfer_progress))
+            print("Can not upload object %s: %s" % (name, str(e)))
+            sys.exit(1)
+
+        return
 
     def download_file(self, obj_name, bucket, dest=None):
 
@@ -202,13 +258,31 @@ class tester:
             print("Can not download object %s: %s" % (obj_name, str(e)))
             sys.exit(1)
 
+    def download_file_thread(self, obj_name, bucket, dest):
+
+        try:
+            if self.awsProfile:
+                thread_s3session = boto3.Session(profile_name=self.awsProfile,)
+            else:
+                thread_s3session = boto3.Session(profile_name="default",)
+        except botocore.exceptions.ProfileNotFound as e:
+            print("Error: %s" % str(e))
+            sys.exit(1)
+        thread_s3 = thread_s3session.client('s3', endpoint_url=self.endPoint, verify=False, config=self.awsConfig)
+
+        try:
+            response = thread_s3.download_file(bucket, obj_name, dest, Callback=self.thread_status_callback)
+        except (ClientError, PermissionError) as e:
+            if self.percentage > 0:
+                print("")
+                print("Transferred %s" % formatSize(round(self.xfer_total * (self.percentage/100))))
+            print("Can not download object %s: %s" % (obj_name, str(e)))
+            sys.exit(1)
+
     def delete_file(self, obj_name, bucket):
 
         try:
             response = self.s3.delete_object(Bucket=bucket, Key=obj_name)
-            self.status_callback(None)
-            if self.current_file == self.opCount:
-                print("")
         except (ClientError) as e:
             print("Can not delete object %s: %s" % (obj_name, str(e)))
             sys.exit(1)
@@ -305,6 +379,59 @@ class tester:
 
         os.unlink(test_file)
 
+    def put_test_thread(self):
+
+        size = self.dataSize
+        count = self.opCount
+        threads = self.threadCount
+        self.xfer_total = count * size
+
+        if self.bucketName is None:
+            print("Error: Bucket name is required.")
+            sys.exit(1)
+
+        if self.verboseFlag:
+            print("Beginning PUT test with %d objects of size %s" % (count, formatSize(size)))
+            print("Total size: %s" % formatSize(self.xfer_total))
+
+        test_file = self.create_test_file(size)
+
+        thread_set = []
+        thread_stat = []
+        q = queue.Queue()
+
+        for i in range(threads):
+            thread_stat.append(0)
+            thread_set.append(0)
+
+        status_thread = threading.Thread(target=self.print_status_thread,args=(q,))
+        status_thread.start()
+
+        while self.current_file < self.opCount:
+            for x in range(threads):
+                if thread_stat[x] == 0:
+                    thread_stat[x] = 1
+                    self.current_file += 1
+                    q.put(self.current_file)
+                    obj_name = self.filePrefix + '-' + str(self.current_file)
+                    thread_set[x] = threading.Thread(target=self.upload_file_thread,
+                                                 args=(test_file, self.bucketName, obj_name,))
+                    thread_set[x].start()
+                if self.current_file == self.opCount:
+                    break
+            time.sleep(1)
+            for y in range(threads):
+                if not thread_set[y].is_alive():
+                    thread_stat[y] = 0
+
+        for y in range(threads):
+            thread_set[y].join()
+
+        self.status_thread_run = 0
+        status_thread.join()
+
+        os.unlink(test_file)
+
     def get_test(self):
 
         count = self.opCount
@@ -327,6 +454,59 @@ class tester:
                 dest_name = self.destDir + '/' + obj_name
             self.download_file(obj_name, self.bucketName, dest=dest_name)
 
+    def get_test_thread(self):
+
+        count = self.opCount
+        threads = self.threadCount
+
+        if self.bucketName is None:
+            print("Error: Bucket name is required.")
+            sys.exit(1)
+
+        self.list_bucket(quiet=True)
+
+        if self.verboseFlag:
+            print("Beginning GET test for %d objects" % count)
+            print("Total size: %s" % formatSize(self.xfer_total))
+
+        thread_set = []
+        thread_stat = []
+        q = queue.Queue()
+
+        for i in range(threads):
+            thread_stat.append(0)
+            thread_set.append(0)
+
+        status_thread = threading.Thread(target=self.print_status_thread,args=(q,))
+        status_thread.start()
+
+        while self.current_file < self.opCount:
+            for x in range(threads):
+                if thread_stat[x] == 0:
+                    thread_stat[x] = 1
+                    self.current_file += 1
+                    q.put(self.current_file)
+                    obj_name = self.filePrefix + '-' + str(self.current_file)
+                    if self.destDir == '/dev/null':
+                        dest_name = '/dev/null'
+                    else:
+                        dest_name = self.destDir + '/' + obj_name
+                    thread_set[x] = threading.Thread(target=self.download_file_thread,
+                                                 args=(obj_name, self.bucketName, dest_name,))
+                    thread_set[x].start()
+                if self.current_file == self.opCount:
+                    break
+            time.sleep(1)
+            for y in range(threads):
+                if not thread_set[y].is_alive():
+                    thread_stat[y] = 0
+
+        for y in range(threads):
+            thread_set[y].join()
+
+        self.status_thread_run = 0
+        status_thread.join()
+
     def delete_test(self):
 
         count = self.opCount
@@ -334,13 +514,55 @@ class tester:
             print("Error: Bucket name is required.")
             sys.exit(1)
 
+        q = queue.Queue()
+
         if self.verboseFlag:
             print("Beginning DELETE test for %d objects" % count)
 
+        status_thread = threading.Thread(target=self.print_status_thread,args=(q,))
+        status_thread.start()
+
         for x in range(count):
             self.current_file = x + 1
+            q.put(self.current_file)
             obj_name = self.filePrefix + '-' + str(self.current_file)
             self.delete_file(obj_name, self.bucketName)
+
+        self.status_thread_run = 0
+        status_thread.join()
+
+    def thread_model(self, op):
+
+        self.threadCount = 1
+        still_searching = True
+        last_time = 0
+        run_thread_count = 0
+        iteration = 1
+
+        while still_searching and self.threadCount <= self.opCount:
+            print("%d) -> Running test pass with threads = %d" % (iteration, self.threadCount))
+            self.current_file = 0
+            self.status_thread_run = 1
+            start_time = datetime.datetime.now()
+            if op == 'put':
+                self.put_test_thread()
+            elif op == 'get':
+                self.get_test_thread()
+            end_time = datetime.datetime.now()
+            time_diff = end_time - start_time
+            if last_time == 0:
+                last_time = time_diff
+                run_thread_count = self.threadCount
+            else:
+                if time_diff > last_time:
+                    still_searching = False
+                else:
+                    last_time = time_diff
+                    run_thread_count = self.threadCount
+            self.threadCount = self.threadCount * 2
+            iteration += 1
+
+        print(" >>> Optimal thread count = %d  -> run time %s <<<" % (run_thread_count, last_time))
 
 def main():
 
@@ -356,11 +578,17 @@ def main():
         test.register_start()
 
     if test.opType == 'put':
-        test.put_test()
+        if runargs.modelFlag:
+            test.thread_model('put')
+        else:
+            test.put_test_thread()
     elif test.opType == 'list':
         test.list_bucket()
     elif test.opType == 'get':
-        test.get_test()
+        if runargs.modelFlag:
+            test.thread_model('get')
+        else:
+            test.get_test_thread()
     elif test.opType == 'delete':
         test.delete_test()
     else:
